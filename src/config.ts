a8f50a513662f75e5c9d2211ef4ede9cfbf55815
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-08-28 16:13:34 +0100

    Initial website config

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,9 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
+    website: 'https://abmu.xyz/', // Site domain
+    title: 'Abdul Muhaymin', // Site title
+    author: 'Abdul Muhaymin', // Author name
     description: 'Minimal blog built by Astro', // Site description
     language: 'en-US' // Default language
   },
@@ -14,10 +14,10 @@ export const themeConfig: ThemeConfig = {
   general: {
     contentWidth: '35rem', // Content area width
     centeredLayout: true, // Use centered layout (false for left-aligned)
-    themeToggle: false, // Show theme toggle button (uses system theme by default)
+    themeToggle: true, // Show theme toggle button (uses system theme by default)
     postListDottedDivider: false, // Show dotted divider in post list
     footer: true, // Show footer
-    fadeAnimation: true // Enable fade animations
+    fadeAnimation: false // Enable fade animations
   },
 
   // DATE SETTINGS ///////////////////////////////////////////////////////////////////////////////////////
@@ -33,6 +33,6 @@ export const themeConfig: ThemeConfig = {
     toc: true, // Show table of contents (when there is enough page width)
     imageViewer: true, // Enable image viewer
     copyCode: true, // Enable copy button in code blocks
-    linkCard: true // Enable link card
+    linkCard: false // Enable link card
   }
 }
--- src/content/about/about.md
+++ src/content/about/about.md
@@ -7,10 +7,8 @@ This content will be displayed at the top of the index page.
 You can leave this empty if you don’t want to show any content.
 -->
 
-A static blog theme based on [Astro](https://astro.build), designed for clarity and focus.
+Currently a BSc Computer Science student at UCL interested in _networks_ and _machine learning_.
 
-With a deliberately minimal design, this layout ensures your content takes center stage. It's built for flexibility, offering customization options that honor its clean and elegant aesthetic.
-
-Effortlessly share your thoughts in _a calm & dustless space._
-
-Check posts for details and view source on [GitHub](https://github.com/the3ash/astro-chiri).
+- [Email](mailto:amuhaymin.2005@gmail.com)
+- [GitHub](https://github.com/abmu)
+- [LinkedIn](https://www.linkedin.com/in/abmu)
